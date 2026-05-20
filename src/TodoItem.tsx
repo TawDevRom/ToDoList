ca3abe76b429bf905f65f29990274d409ae0e89d
Роман <popov06.07@mail.ru>
2026-05-20 14:49:37 +0300

    Изменение дизайна и google шрифты

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -12,11 +12,12 @@ type TodoItemProps = {
 export function TodoItem({ task, editingId, editingValue,
     onToggle, onDelete, onEdit, onSave, setEditingValue }: TodoItemProps) {
     return (
-        <li>
+        <li className="flex justify-between items-center gap-3 bg-gray-50 border border-gray-300 rounded-xl px-4 py-3 mb-0.5">
             <input
                 type="checkbox"
                 checked={task.done}
                 onChange={() => onToggle(task.id)}
+                className="w-4 h-4 accent-blue-500 cursor-pointer"
             />
 
             {editingId === task.id ? (
@@ -27,27 +28,27 @@ export function TodoItem({ task, editingId, editingValue,
                     onKeyDown={(e) => {
                         if (e.key === 'Enter') onSave(task.id);
                     }}
+                    className={``}
                 />
             ) : (
-                <span style={{ textDecoration: task.done ? 'line-through' : 'none' }}>
-                    {task.title} ({task.priority})
+                <span className={`flex-1 text-sm ${task.done ? 'line-through text-gray-400' : 'text-gray-700'}`}>
+                    {task.title}
+                    <span className="ml-2 text-xs text-gray-400">({task.priority})</span>
                 </span>
             )}
-
             {editingId === task.id ? (
-                <button onClick={() => onSave(task.id)}>Сохранить</button>
+                <button onClick={() => onSave(task.id)}>&#x1F5AC;</button>
             ) : (
-                <button onClick={() => onEdit(task)}>Редактировать</button>
+                <button onClick={() => onEdit(task)} className="inline-block -scale-x-100">✎</button>
             )
             }
 
             {editingId === task.id ? (
                 <div></div>
             ) : (
-                <button onClick={() => onDelete(task.id)}>x</button>
+                <button onClick={() => onDelete(task.id)}>&#128465;</button>
             )
             }
-
         </li>
     )
 }
